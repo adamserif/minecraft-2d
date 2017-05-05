801b7e50c6793fe43e123a289c1c49ea3251dbdc
Adam Rahman <mrahman@wesleyan.edu>
2017-05-04 20:41:38 -0400

    Basic pygame setup

---
+++ game.py
@@ -0,0 +1,21 @@
+import pygame, sys
+
+from pygame.locals import *
+
+pygame.init()
+
+
+DISPLAYSURF = pygame.display.set_mode((300,300))
+
+pygame.display.set_caption('Tile Heaven')
+
+while True:
+
+	for event in pygame.event.get():
+
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+		pygame.display.update()
+
